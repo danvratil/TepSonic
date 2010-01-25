8846156f866e4d04013cc85f7bf7e9deda6ddccb
Dan Vratil <vratil@progdansoft.com>
2010-01-26 00:50:00 +0100

    Bump to 0.8, first working release

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     a.setApplicationName("TepSonic");
     a.setOrganizationDomain("ProgDan Soft");
-    a.setApplicationVersion("0.60");
+    a.setApplicationVersion("0.8");
     MainWindow w;
     w.show();
     return a.exec();
