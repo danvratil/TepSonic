529f2ddd6185eea9cac3e7c82234425abdfa855e
Dan Vratil <vratil@progdansoft.com>
2010-02-05 21:37:59 +0100

    Change the way that SqlDatabase connection is used in CollectionBuilder

--- collectionbuilder.cpp
+++ collectionbuilder.cpp
@@ -18,19 +18,18 @@ void CollectionBuilder::run()
     qDebug() << "Updating CollectionBrowser...";
     DatabaseManager dbManager("updateCollectionBrowserConnection");
     dbManager.connectToDB();
-    QSqlDatabase sqlConn = dbManager.sqlDb;
 
     _model->removeRows(0,_model->rowCount(QModelIndex()),QModelIndex());
 
     QModelIndex albumsParent;
     QModelIndex tracksParent;
-    QSqlQuery artistsQuery("SELECT `artist` FROM `Tracks` GROUP BY `artist` ORDER BY `artist` DESC",sqlConn);
+    QSqlQuery artistsQuery("SELECT `artist` FROM `Tracks` GROUP BY `artist` ORDER BY `artist` DESC",dbManager.sqlDb);
     while (artistsQuery.next()) {
         albumsParent = _model->addRow(QModelIndex(),artistsQuery.value(0).toString(),QString());
-        QSqlQuery albumsQuery("SELECT `album` FROM `Tracks` WHERE `artist`='"+artistsQuery.value(0).toString()+"' GROUP BY `album` ORDER BY `album` DESC;",sqlConn);
+        QSqlQuery albumsQuery("SELECT `album` FROM `Tracks` WHERE `artist`='"+artistsQuery.value(0).toString()+"' GROUP BY `album` ORDER BY `album` DESC;",dbManager.sqlDb);
         while (albumsQuery.next()) {
             tracksParent = _model->addChild(albumsParent,albumsQuery.value(0).toString(),QString());
-            QSqlQuery tracksQuery("SELECT `title`,`filename` FROM `Tracks` WHERE `album`='"+albumsQuery.value(0).toString()+"' AND `artist`='"+artistsQuery.value(0).toString()+"' ORDER BY `trackNo` DESC;",sqlConn);
+            QSqlQuery tracksQuery("SELECT `title`,`filename` FROM `Tracks` WHERE `album`='"+albumsQuery.value(0).toString()+"' AND `artist`='"+artistsQuery.value(0).toString()+"' ORDER BY `trackNo` DESC;",dbManager.sqlDb);
             while (tracksQuery.next()) {
                 _model->addChild(tracksParent,tracksQuery.value(0).toString(),tracksQuery.value(1).toString());
             }
