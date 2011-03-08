71b32aa5eded8c51b5793d4766f818a56d8003ab
Dan Vrátil <vratil@progdansoft.com>
2011-03-09 00:23:43 +0100

    Fix build on Windows

--- player/pluginsmanager.cpp
+++ player/pluginsmanager.cpp
@@ -66,7 +66,8 @@ void PluginsManager::loadPlugins()
        Putting this folder first will also result in preferring these plugins before
        same-called plugins installed somewhere in system */
 #ifdef Q_WS_WIN
-    pluginsDirs << qApp->applicationDirPath()+QDir::separator()+"plugins";
+    pluginsDirs << QDir::toNativeSeparators(qApp->applicationDirPath())+QDir::separator()+"plugins";
+    QCoreApplication::addLibraryPath(QDir::toNativeSeparators(qApp->applicationDirPath()));
 #endif
 #ifdef Q_WS_MAC
     #ifdef APPLEBUNDLE
