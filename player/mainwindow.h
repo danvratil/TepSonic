7baedd7fb59836bafab9fef753aa82ca4e9b4e2a
Dan Vrátil <dan@progdan.cz>
2013-12-27 21:02:29 +0100

    Fix copyright header in FileSystemWidget and MainWindow

--- player/mainwindow.h
+++ player/mainwindow.h
@@ -1,6 +1,6 @@
 /*
  * TEPSONIC
- * Copyright 2009 Dan Vratil <vratil@progdansoft.com>
+ * Copyright 2013 Daniel Vrátil <dan@progdan.cz>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
